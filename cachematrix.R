ac1c1f99cf0bd700bd1aa1ee6be26bd8bd6ff073
RUdouby <dcleary55@gmail.com>
2019-10-21 23:24:21 -0400

    wrote code for the makeCacheMatrix and cahceSolve matrix to calculate a cached inverse  of a matrix and call it if already cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## the makeCacheMatrix function will return a list of functions with a matrix cached if previously run
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL                               ## initialize m as NULL
+        set <- function(y) {
+                x <<- y                         ## creates the given matrix y in the function environment
+                m <<- NULL
+        }
+        get <- function() x                     ## function that returns the matrix
+        setinv <- function(j) m <<- j           ## sets the inverse
+        getinv <- function() m                  ## returns invere function
+        list(set = set, get = get, setinv = setinv, getinv = getinv)  ##returns list to run functions
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()                         ## x is a list so this pulls getinv() which is the function that returns inverse
+        if(!is.null(m)) { 
+                message("getting cached data")
+                return(m)                       ## checks if m is not NULL and returns it if so
+        }
+        data <- x$get()                         ## if m is NULL it goes to this line which pulls in y, original matrix
+        m <- solve(data)                        ## calculates inverse of y
+        x$setinv(m)                             ## sets the inverse of y to be cached matrix
+        m                                       ## return m
 }
